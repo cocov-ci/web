3354f2c2aa2ce3f5d089444d2e61520e7a086bcc
robsoncoelho <robson.amc@gmail.com>
2022-12-20 19:50:33 +0000

    Setting up fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
+
+const satoshi = localFont({ src: '../public/fonts/Satoshi.ttf' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={satoshi.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
