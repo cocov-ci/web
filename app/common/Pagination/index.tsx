09f7f3fa5a9786a1d909c593be294dcc02074d81
robsoncoelho <robson.amc@gmail.com>
2023-01-06 17:07:28 +0000

    Pagination component static versin

--- app/common/Pagination/index.tsx
+++ app/common/Pagination/index.tsx
@@ -11,13 +11,42 @@ interface PaginationProps {
   currentPage: number
 }
 
+interface ArrowProps {
+  direction: 'previous' | 'next'
+}
+
 const Pagination = ({ total, currentPage }: PaginationProps) => {
   const [active, setActive] = useState<number>(currentPage)
+  const firstPage = active === 1
+  const lastPage = active === total
+
+  const Arrow = ({ direction }: ArrowProps) => {
+    if (direction === 'previous') {
+      return (
+        <ChevronFirst
+          className={classNames(styles.arrow, {
+            [styles.disabled]: firstPage,
+          })}
+          onClick={() => !firstPage && setActive(active - 1)}
+        />
+      )
+    } else {
+      return (
+        <ChevronLast
+          className={classNames(styles.arrow, {
+            [styles.disabled]: lastPage,
+          })}
+          onClick={() => !lastPage && setActive(active + 1)}
+        />
+      )
+    }
+  }
 
   return (
     <div className={styles.pagination}>
       <ul className={styles.pages}>
-        <ChevronFirst />
+        <Arrow direction="previous" />
+
         {[...Array(total)].map((item, index): React.ReactElement => {
           const pageNumber = index + 1
 
@@ -33,7 +62,8 @@ const Pagination = ({ total, currentPage }: PaginationProps) => {
             </li>
           )
         })}
-        <ChevronLast />
+
+        <Arrow direction="next" />
       </ul>
     </div>
   )
