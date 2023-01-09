c214e0142536b986c6a2a5373264b19ba6443159
Victor Gama <hey@vito.io>
2023-01-09 19:33:58 -0300

    feat(stats): Update styles, add stories

--- app/common/Stats/Utils.tsx
+++ app/common/Stats/Utils.tsx
@@ -3,20 +3,26 @@ interface GradientProps {
   isCoverage: boolean
 }
 
+// Arrays containing gradient start, gradient stop, and border
+const chartColors = {
+  coverage: ['rgba(17, 150, 255, 0.36)', 'rgba(255, 255, 255, 1)', '#1196FF'],
+  issues: ['rgba(205,29,141,0.36)', '#E8E8E8', '#CD1D8D'],
+}
+
 export const generateGradient = ({ ctx, isCoverage }: GradientProps) => {
-  const gradient = ctx.createLinearGradient(0, 35, 0, isCoverage ? 60 : 80)
-  const chartColor = isCoverage
-    ? 'rgba(17, 150, 255, 0.1)'
-    : 'rgba(205, 29, 141, 0.1)'
-  gradient.addColorStop(0, chartColor)
-  gradient.addColorStop(1, 'rgba(232, 232, 232, 0.1)')
+  const gradient = ctx.createLinearGradient(0, 35, 0, 80)
+  const [colorStart, colorStop] = isCoverage
+    ? chartColors.coverage
+    : chartColors.issues
+
+  gradient.addColorStop(0, colorStart)
+  gradient.addColorStop(1, colorStop)
 
   return gradient
 }
 
-export const getBorderColor = (isCoverage: boolean) => {
-  return isCoverage ? 'rgba(17, 150, 255, 0.23)' : 'rgba(205, 29, 141, 0.23)'
-}
+export const getBorderColor = (isCoverage: boolean) =>
+  (isCoverage ? chartColors.coverage : chartColors.issues)[2]
 
 export const options = {
   pointStyle: false,
