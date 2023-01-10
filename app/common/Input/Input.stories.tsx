b6c811d040893bcea947ac370249f5b7ab2fd3db
Victor Gama <hey@vito.io>
2023-01-10 01:55:43 -0300

    feat(input): Initial implementation

---
+++ app/common/Input/Input.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { LucideSearch } from 'lucide-react'
+
+import Input from './index'
+
+const meta: Meta<typeof Input> = {
+  title: 'Common/Input',
+  component: Input,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Input>
+
+export const Default: Story = {
+  args: {
+    type: 'text',
+  },
+}
+
+export const WithPlaceholder: Story = {
+  args: {
+    type: 'text',
+    placeholder: 'Placeholder',
+  },
+}
+
+export const WithLabel: Story = {
+  args: {
+    label: 'Email:',
+    type: 'text',
+    placeholder: 'paul.appleseed@example.com',
+  },
+}
+
+export const WithLoading: Story = {
+  args: {
+    label: 'Name:',
+    type: 'text',
+    value: 'The name',
+    loading: true,
+  },
+}
+
+export const WithIcon: Story = {
+  args: {
+    type: 'text',
+    placeholder: 'Type to Search...',
+    loading: true,
+    icon: LucideSearch,
+  },
+}
+
+export const Errored: Story = {
+  args: {
+    type: 'text',
+    errored: true,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    label: 'Test:',
+    type: 'text',
+    disabled: true,
+  },
+}
---
+++ app/common/Input/index.tsx
@@ -0,0 +1,75 @@
+import classNames from 'classnames'
+import { LucideIcon } from 'lucide-react'
+import React, { ChangeEventHandler, useRef } from 'react'
+
+import Loading from '../Loading'
+
+import styles from './Input.module.scss'
+
+interface InputProps {
+  icon?: LucideIcon
+  className?: string
+  value?: string
+  disabled?: boolean
+  placeholder?: string
+  onChange?: ChangeEventHandler
+  errored?: boolean
+  type?: string
+  label?: string
+  loading?: boolean
+}
+
+const Input = ({
+  className,
+  value,
+  disabled,
+  placeholder,
+  onChange,
+  errored,
+  type,
+  label,
+  loading,
+  icon: Icon,
+}: InputProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  return (
+    <div className={styles.base}>
+      {label && (
+        <label
+          className={classNames(styles.label, className, {
+            [styles.labelDisabled]: disabled === true,
+          })}
+          onClick={() => inputRef.current && inputRef.current.focus()}
+        >
+          {label}
+        </label>
+      )}
+      {Icon && (
+        <div className={styles.iconContainer}>
+          <Icon size={18} />
+        </div>
+      )}
+      <input
+        className={classNames(styles.input, className, {
+          [styles.errored]: errored,
+          [styles.withIcon]: !!Icon,
+          [styles.withSpinner]: !!loading,
+        })}
+        disabled={disabled === true}
+        onChange={onChange}
+        placeholder={placeholder}
+        ref={inputRef}
+        type={type}
+        value={value}
+      />
+      {loading && (
+        <div className={classNames(styles.loading)}>
+          <Loading size={20} tiny={true} type="spinner" />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Input
