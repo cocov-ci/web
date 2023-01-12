bdb6ec8a9a01776d90de00fdb9954f14f6d472cd
Victor Gama <hey@vito.io>
2023-01-12 13:48:46 -0300

    feat(CommitHeader): Add Loading component

---
+++ app/common/CommitHeader/Loading/index.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import Loading from 'app/common/Loading'
+import randomBetween from 'utils/randomBetween'
+
+import styles from '../CommitHeader.module.scss'
+
+export type CommitHeaderProps = {
+  className?: string
+}
+
+const LoadingComponent = ({ className }: CommitHeaderProps) => {
+  return (
+    <div className={classNames(styles.base, className)}>
+      <div className={styles.avatarWrapper}>
+        <Loading
+          alignment="left"
+          className={styles.skeleton}
+          height="25px"
+          type="skeleton"
+          width="25px"
+        />
+      </div>
+
+      <div className={styles.infoWrapper}>
+        <div className={styles.commitMeta}>
+          <Loading
+            alignment="left"
+            height="21px"
+            type="skeleton"
+            width={`${randomBetween(206, 400)}px`}
+          />
+        </div>
+        <div className={styles.commitTitle}>
+          <Loading
+            alignment="left"
+            height="22px"
+            type="skeleton"
+            width={`${randomBetween(32, 400)}px`}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingComponent
