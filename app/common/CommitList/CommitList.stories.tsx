48fabaec7c8e2747b314a6cc4bc15527585b4ca3
Victor Gama <hey@vito.io>
2023-01-12 13:51:41 -0300

    feat: Implement CommitList component

---
+++ app/common/CommitList/CommitList.stories.tsx
@@ -0,0 +1,70 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CommitList from './index'
+
+const meta: Meta<typeof CommitList> = {
+  title: 'Common/CommitList',
+  component: CommitList,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof CommitList>
+
+export const Default: Story = {
+  args: {
+    paginationData: {
+      total: 10,
+      currentPage: 2,
+      onPageClick: () => void 0,
+    },
+    commits: [
+      {
+        commitInfo: {
+          headURL: 'https://github.com/cocov-ci/web',
+          avatarURL:
+            'https://avatars.githubusercontent.com/u/118852412?s=200&v=4',
+          headSHA: '62bad0cbadbeefc0c0f',
+          commitMessage:
+            'fix(rollout-restart): Ensure to keep watching rollout status',
+          username: 'cocov-ci',
+          timestamp: new Date(Date.parse('2022-11-22T21:25:31Z')),
+          readonly: true,
+        },
+        coverage: 100,
+        issues: 9999,
+      },
+      {
+        commitInfo: {
+          headURL: 'https://github.com/cocov-ci/web',
+          avatarURL:
+            'https://avatars.githubusercontent.com/u/118852412?s=200&v=4',
+          headSHA: '62bad0cbadbeefc0c0f1',
+          commitMessage:
+            'fix(rollout-restart): Ensure to keep watching rollout status',
+          username: 'cocov-ci',
+          timestamp: new Date(Date.parse('2022-11-22T21:25:31Z')),
+          readonly: true,
+        },
+        coverage: 100,
+        issues: 9999,
+      },
+      {
+        commitInfo: {
+          headURL: 'https://github.com/cocov-ci/web',
+          avatarURL:
+            'https://avatars.githubusercontent.com/u/118852412?s=200&v=4',
+          headSHA: '62bad0cbadbeefc0c0f2',
+          commitMessage:
+            'fix(rollout-restart): Ensure to keep watching rollout status',
+          username: 'cocov-ci',
+          timestamp: new Date(Date.parse('2022-11-22T21:25:31Z')),
+          readonly: true,
+        },
+        coverage: 100,
+        issues: 9999,
+      },
+    ],
+  },
+}
