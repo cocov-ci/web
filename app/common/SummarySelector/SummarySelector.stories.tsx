9dcf1ab73a4c6b5e8df296c28cc302638c230efa
Victor Gama <hey@vito.io>
2023-01-15 15:30:05 -0300

    feat: Implement SummarySelector component

---
+++ app/common/SummarySelector/SummarySelector.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import SummarySelector from './index'
+
+const meta: Meta<typeof SummarySelector> = {
+  title: 'Common/SummarySelector',
+  component: SummarySelector,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof SummarySelector>
+
+export const Default: Story = {
+  args: {
+    branchName: 'master',
+  },
+}
