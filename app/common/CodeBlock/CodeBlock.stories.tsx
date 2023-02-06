01b823dcda9f9372da94ff0c3aa8bde555175f46
robsoncoelho <robson.amc@gmail.com>
2023-02-06 18:38:57 +0000

    Update issues type

--- app/common/CodeBlock/CodeBlock.stories.tsx
+++ app/common/CodeBlock/CodeBlock.stories.tsx
@@ -58,45 +58,43 @@ export const Default: Story = {
 
 export const LongMessage: Story = {
   args: {
-    issue: {
-      objects: [
-        {
-          kind: 'line',
-          line: 1,
-          source:
-            '<pre>    <span class="k">def</span> <span class="nf">repo_path</span><span class="p">(</span><span class="n">repo</span><span class="p">)</span>\n</pre>',
-        },
-        {
-          kind: 'line',
-          line: 2,
-          source:
-            '<pre>      <span class="n">base</span> <span class="o">=</span> <span class="n">base_path</span>\n</pre>',
-        },
-        {
-          kind: 'line',
-          line: 3,
-          source:
-            '<pre>      <span class="nb">name</span> <span class="o">=</span> <span class="no">Digest</span><span class="o">::</span><span class="no">SHA1</span><span class="p">.</span><span class="nf">hexdigest</span><span class="p">(</span><span class="n">repo</span><span class="p">)</span>\n</pre>',
-        },
-        {
-          kind: 'warn',
-          text: '$METHOD is a state-changing MVC method that does not validate the antiforgery token or do strict content-type checking. State-changing controller methods should either enforce antiforgery tokens or do strict content-type checking to prevent simple HTTP request types from bypassing CORS preflight controls.',
-          padding: '      ',
-        },
-        {
-          kind: 'line',
-          line: 4,
-          source:
-            '<pre>      <span class="k">if</span> <span class="n">base</span><span class="p">.</span><span class="nf">is_a?</span> <span class="no">Pathname</span>\n</pre>',
-        },
-        {
-          kind: 'line',
-          line: 5,
-          source:
-            '<pre>        <span class="n">base</span><span class="p">.</span><span class="nf">join</span><span class="p">(</span><span class="nb">name</span><span class="p">)</span>\n</pre>',
-        },
-      ],
-    },
+    issue: [
+      {
+        type: 'line',
+        line: 1,
+        source:
+          '<pre>    <span class="k">def</span> <span class="nf">repo_path</span><span class="p">(</span><span class="n">repo</span><span class="p">)</span>\n</pre>',
+      },
+      {
+        type: 'line',
+        line: 2,
+        source:
+          '<pre>      <span class="n">base</span> <span class="o">=</span> <span class="n">base_path</span>\n</pre>',
+      },
+      {
+        type: 'line',
+        line: 3,
+        source:
+          '<pre>      <span class="nb">name</span> <span class="o">=</span> <span class="no">Digest</span><span class="o">::</span><span class="no">SHA1</span><span class="p">.</span><span class="nf">hexdigest</span><span class="p">(</span><span class="n">repo</span><span class="p">)</span>\n</pre>',
+      },
+      {
+        type: 'warn',
+        text: '$METHOD is a state-changing MVC method that does not validate the antiforgery token or do strict content-type checking. State-changing controller methods should either enforce antiforgery tokens or do strict content-type checking to prevent simple HTTP request types from bypassing CORS preflight controls.',
+        padding: '      ',
+      },
+      {
+        type: 'line',
+        line: 4,
+        source:
+          '<pre>      <span class="k">if</span> <span class="n">base</span><span class="p">.</span><span class="nf">is_a?</span> <span class="no">Pathname</span>\n</pre>',
+      },
+      {
+        type: 'line',
+        line: 5,
+        source:
+          '<pre>        <span class="n">base</span><span class="p">.</span><span class="nf">join</span><span class="p">(</span><span class="nb">name</span><span class="p">)</span>\n</pre>',
+      },
+    ],
   },
 }
 
