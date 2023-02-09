dd57ab1cc647b13eac073ba800b2da85efcd33e3
robsoncoelho <robson.amc@gmail.com>
2023-02-09 21:39:02 +0000

    Add coverage status messages

--- app/repos/[repositoryName]/commits/[commitSha]/coverage/[fileId]/page.tsx
+++ app/repos/[repositoryName]/commits/[commitSha]/coverage/[fileId]/page.tsx
@@ -2,7 +2,9 @@ interface FileId {
   params: { repositoryName: string; commitSha: string; fileId: string }
 }
 
-const FileId = ({ params: { repositoryName, commitSha, fileId } }: FileId) => {
+const FileId = async ({
+  params: { repositoryName, commitSha, fileId },
+}: FileId) => {
   return (
     <div>
       {repositoryName} {commitSha} {fileId}
--- app/repos/[repositoryName]/commits/[commitSha]/coverage/page.tsx
+++ app/repos/[repositoryName]/commits/[commitSha]/coverage/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { AlertTriangle, Cog, Send } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
+import Alert from 'app/common/Alert'
 import Box from 'app/common/Box'
 import FileList from 'app/common/FileList'
 import CommitHeader from 'app/repos/[repositoryName]/commits/[commitSha]/CommitHeader'
@@ -46,12 +48,38 @@ const CommitsCoverage = ({
           repositoryName={repositoryName}
         />
         <div className={styles.content}>
-          <FileList
-            className={styles.fileList}
-            commitSha={commitSha}
-            files={data?.files}
-            repositoryName={repositoryName}
-          />
+          {data?.status === 'waiting' && (
+            <Alert
+              className={styles.alert}
+              description="Information about this commit's coverage will be displayed here after this instance receives it."
+              icon={Send}
+              title="Waiting for Data..."
+            />
+          )}
+          {(data?.status === 'queued' || data?.status === 'processing') && (
+            <Alert
+              className={styles.alert}
+              description="This instance is currently processing received coverage data. It should be available here in a few seconds."
+              icon={Cog}
+              title="Processing Data..."
+            />
+          )}
+          {data?.status === 'errored' && (
+            <Alert
+              className={styles.alert}
+              description="A problem prevented this instance from successfully processing the received coverage data. Please refer to the instance's log for more information."
+              icon={AlertTriangle}
+              title="Internal Error"
+            />
+          )}
+          {data?.status === 'processed' && (
+            <FileList
+              className={styles.fileList}
+              commitSha={commitSha}
+              files={data?.files}
+              repositoryName={repositoryName}
+            />
+          )}
         </div>
       </Box>
     </div>
