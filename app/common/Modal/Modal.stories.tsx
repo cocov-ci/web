f0dfea0f9011655e5724abba1d53ec33edd47992
Victor Gama <hey@vito.io>
2023-02-23 15:45:19 -0300

    refactor: Tweak modal background animation

--- app/common/Modal/Modal.stories.tsx
+++ app/common/Modal/Modal.stories.tsx
@@ -1,15 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+
+import BackgroundCats from 'stories/BackgroundCats'
+import { satoshi } from 'utils/fonts'
 
 import Modal from './index'
 
 const meta: Meta<typeof Modal> = {
   title: 'Common/Modal',
-  component: args => (
-    <div style={{ height: '300px' }}>
-      <Modal {...args} />
-    </div>
-  ),
+  component: Modal,
   tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div
+        className={satoshi.className}
+        style={{
+          position: 'relative',
+          margin: '0',
+          padding: '0',
+          height: '100vh',
+          width: '100vw',
+        }}
+      >
+        <BackgroundCats />
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 export default meta
