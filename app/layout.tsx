b1426b31305085f3bd2cc12e1df1e9055fcdf88d
robsoncoelho <robson.amc@gmail.com>
2023-07-04 17:12:24 +0100

    feat: add cocov-api-beacon metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+import { Metadata } from 'next'
 import React from 'react'
 
 import 'styles/globals.scss'
@@ -11,6 +10,13 @@ import BannerProvider from 'context/BannerContext'
 import ModalProvider from 'context/ModalContext'
 import SegmentsProvider from 'context/SegmentsContext'
 import { satoshi } from 'utils/fonts'
+
+export const metadata: Metadata = {
+  title: 'Cocov',
+  other: {
+    'cocov-api-beacon': process.env.NEXT_PUBLIC_COCOV_API_URL as string,
+  },
+}
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
