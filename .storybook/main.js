67c3dcc9f1669a81e9262283f7883e71e3646930
Victor Gama <hey@vito.io>
2023-07-22 00:31:05 -0300

    fix(storybook): Bump to Yarn 2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,18 @@
 module.exports = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../app/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)', '../app/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: 'tag'
   },
-  staticDirs: [
-    {
-      from: '../public/',
-      to: 'public/',
-    },
-    {
-      from: '../public/icons',
-      to: '/icons',
-    },
-  ],
-}
+  staticDirs: [{
+    from: '../public/',
+    to: 'public/'
+  }, {
+    from: '../public/icons',
+    to: '/icons'
+  }]
+};
